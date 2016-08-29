b0006638a57fe5e48dfd2d0d3b879ac190be554c
phisrood <kym1316@naver.com>
2016-08-29 12:55:02 +0900

    중간저장

--- studentManagement/src/main/java/com/korea/memberManage/controller/MemberManageController.java
+++ studentManagement/src/main/java/com/korea/memberManage/controller/MemberManageController.java
@@ -1,8 +1,19 @@
 package com.korea.memberManage.controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Controller
 public class MemberManageController {
@@ -11,16 +22,36 @@ public class MemberManageController {
 	//학생정보조회
 	@RequestMapping(value="/emp/stuInfoList", method=RequestMethod.GET)
 	public String stuInfoList(){
-		String url="";
+		String url="/emp/stuInfoList";
 		
+	
 		return url;
 	}
 	
 	//학생정보등록
-	@RequestMapping(value="/emp/stuInfoInsert", method=RequestMethod.GET)
-	public String stuInfoInsert(){
-		String url="";
-		
+	@RequestMapping(value="/emp/stuInfoInsert", method=RequestMethod.POST)
+	public String stuInfoInsert(MultipartHttpServletRequest request){
+		String url="/emp/stuInfoInsert";
+		File file = (File) request.getFile("studentList");
+		Workbook workbook;
+		try {
+			workbook = WorkbookFactory.create(file);
+			int rowindex = 0;
+			int columnindex = 0;
+			// 시트 수 (첫번째에만 존재하므로 0을 준다)
+			// 만약 각 시트를 읽기위해서는 FOR문을 한번더 돌려준다
+			
+			Sheet sheet = workbook.getSheetAt(0);
+			// 행의 수
+			readSheet(columnindex, sheet);
+			
+		} catch (InvalidFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return url;
 	}
 	
@@ -116,4 +147,57 @@ public class MemberManageController {
 		return url;
 	}
 	
+	
+	private void readSheet(int columnindex, Sheet sheet) {
+		int rowindex;
+		int rows = sheet.getPhysicalNumberOfRows();
+
+		for (rowindex = 2; rowindex < rows; rowindex++) {
+			// 행을읽는다
+			Row row = sheet.getRow(rowindex);
+			columnindex = readRow(columnindex, row);
+		}
+	}
+
+	private int readRow(int columnindex, Row row) {
+		if (row != null) {
+			// 셀의 수
+			int cells = row.getPhysicalNumberOfCells();
+			for (columnindex = 0; columnindex < cells; columnindex++) {
+				// 셀값을 읽는다
+				System.out.print(readCell(columnindex, row)+"\t");
+			}
+			System.out.println();
+		}
+		return columnindex;
+	}
+
+	private String readCell(int columnindex, Row row) {
+		Cell cell = row.getCell(columnindex);
+		String value = "";
+		// 셀이 빈값일경우를 위한 널체크
+		if (cell == null) {
+			return "없음";
+		} else {
+			// 타입별로 내용 읽기
+			switch (cell.getCellType()) {
+			case Cell.CELL_TYPE_FORMULA:
+				value = cell.getCellFormula();
+				break;
+			case Cell.CELL_TYPE_NUMERIC:
+				value = cell.getNumericCellValue() + "";
+				break;
+			case Cell.CELL_TYPE_STRING:
+				value = cell.getStringCellValue() + "";
+				break;
+			case Cell.CELL_TYPE_BLANK:
+				value = cell.getBooleanCellValue() + "";
+				break;
+			case Cell.CELL_TYPE_ERROR:
+				value = cell.getErrorCellValue() + "";
+				break;
+			}
+		}
+		return value;
+	}
 }
