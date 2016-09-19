d9f80f2d82d4a6c8d3fe4b14d10a0429a581019a
donheeHan <hdh2688@naver.com>
2016-09-19 13:47:07 +0900

    wwd

---
+++ studentManagement/src/main/java/com/korea/dto/Lecture_Chart_ViewVO.java
@@ -0,0 +1,89 @@
+package com.korea.dto;
+
+public class Lecture_Chart_ViewVO {
+	private String lec_no;
+	private String lec_lb_no;
+	private String lec_ln_pro_use_id;
+	private String lec_makesemester;
+	private String lec_makeyear;
+	private String lec_placement;
+	private String lec_persons;
+	private String lec_persons_count;
+	private String lc_no;
+	private String lc_appraisalquestion;
+	private String lc_questionscore;
+	private String lc_cnt;
+	public String getLec_no() {
+		return lec_no;
+	}
+	public void setLec_no(String lec_no) {
+		this.lec_no = lec_no;
+	}
+	public String getLec_lb_no() {
+		return lec_lb_no;
+	}
+	public void setLec_lb_no(String lec_lb_no) {
+		this.lec_lb_no = lec_lb_no;
+	}
+	public String getLec_ln_pro_use_id() {
+		return lec_ln_pro_use_id;
+	}
+	public void setLec_ln_pro_use_id(String lec_ln_pro_use_id) {
+		this.lec_ln_pro_use_id = lec_ln_pro_use_id;
+	}
+	public String getLec_makesemester() {
+		return lec_makesemester;
+	}
+	public void setLec_makesemester(String lec_makesemester) {
+		this.lec_makesemester = lec_makesemester;
+	}
+	public String getLec_makeyear() {
+		return lec_makeyear;
+	}
+	public void setLec_makeyear(String lec_makeyear) {
+		this.lec_makeyear = lec_makeyear;
+	}
+	public String getLec_placement() {
+		return lec_placement;
+	}
+	public void setLec_placement(String lec_placement) {
+		this.lec_placement = lec_placement;
+	}
+	public String getLec_persons() {
+		return lec_persons;
+	}
+	public void setLec_persons(String lec_persons) {
+		this.lec_persons = lec_persons;
+	}
+	public String getLec_persons_count() {
+		return lec_persons_count;
+	}
+	public void setLec_persons_count(String lec_persons_count) {
+		this.lec_persons_count = lec_persons_count;
+	}
+	public String getLc_no() {
+		return lc_no;
+	}
+	public void setLc_no(String lc_no) {
+		this.lc_no = lc_no;
+	}
+	public String getLc_appraisalquestion() {
+		return lc_appraisalquestion;
+	}
+	public void setLc_appraisalquestion(String lc_appraisalquestion) {
+		this.lc_appraisalquestion = lc_appraisalquestion;
+	}
+	public String getLc_questionscore() {
+		return lc_questionscore;
+	}
+	public void setLc_questionscore(String lc_questionscore) {
+		this.lc_questionscore = lc_questionscore;
+	}
+	public String getLc_cnt() {
+		return lc_cnt;
+	}
+	public void setLc_cnt(String lc_cnt) {
+		this.lc_cnt = lc_cnt;
+	}
+	
+}
