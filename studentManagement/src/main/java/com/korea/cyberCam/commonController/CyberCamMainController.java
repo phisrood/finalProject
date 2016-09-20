6b35d1e0c89fbf415021e72bd451060cdfa5c286
donheeHan <hdh2688@naver.com>
2016-09-20 18:37:53 +0900

    사캠 메인 완료

--- studentManagement/src/main/java/com/korea/cyberCam/commonController/CyberCamMainController.java
+++ studentManagement/src/main/java/com/korea/cyberCam/commonController/CyberCamMainController.java
@@ -15,17 +15,27 @@ package com.korea.cyberCam.commonController;
  * Copyright (c) 2016 by DDIT  All right reserved
  * </pre>
  */
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.korea.cyberCam.noticeBBS.service.CyberCamNoticeBBSService;
+import com.korea.dto.Cyber_LectureNoticeViewVO;
 import com.korea.dto.UsersVO;
 
 @Controller
 public class CyberCamMainController {
+	
+	@Autowired
+	CyberCamNoticeBBSService noticeService;
+	
 	/**
 	 * 개인 정보 조회
 	 * @param
@@ -34,8 +44,10 @@ public class CyberCamMainController {
 	 */
 	//사이버캠퍼스 메인
 	@RequestMapping(value={"/cyberCampus/stu/cyberClassMain","/cyberCampus/pro/cyberClassMain"}, method=RequestMethod.POST)
-	public String cyberClassMainByStu(HttpSession session,String pro_lec_no,@RequestParam(value="lec_no" ,defaultValue="0")int stu_lec_no){
+	public String cyberClassMainByStu(HttpSession session,String pro_lec_no,@RequestParam(value="lec_no" ,defaultValue="0")int stu_lec_no,
+			Model model){
 		String url="/cyberCampus/common/cyberClassMain";
+		int lec_no = 0;
 		
 		// 세션
 		UsersVO user = (UsersVO) session.getAttribute("loginUser");
@@ -48,6 +60,16 @@ public class CyberCamMainController {
 			System.out.println("교수");
 			System.out.println(session.getAttribute("pro_lec_no"));
 		}
+		
+		if(pro_lec_no == null){
+			lec_no = stu_lec_no;
+		}else if(stu_lec_no == 0){
+			lec_no = Integer.parseInt(pro_lec_no);
+		}
+		
+		List<Cyber_LectureNoticeViewVO> noticeList = noticeService.getNoticeBBSList(lec_no);
+		
+		model.addAttribute("noticeList", noticeList);
 		
 		return url;
 	}
