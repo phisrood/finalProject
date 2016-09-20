a898a619d15399753cf3231ea76043da4c3a5362
donheeHan <hdh2688@naver.com>
2016-09-20 19:02:40 +0900

    j

--- studentManagement/src/main/java/com/korea/cyberCam/commonController/CyberCamMainController.java
+++ studentManagement/src/main/java/com/korea/cyberCam/commonController/CyberCamMainController.java
@@ -53,12 +53,8 @@ public class CyberCamMainController {
 		UsersVO user = (UsersVO) session.getAttribute("loginUser");
 		if(user.getAuthority().equals("ROLE_STU")){
 			session.setAttribute("stu_lec_no", stu_lec_no);
-			System.out.println("학생");
-			System.out.println(session.getAttribute("stu_lec_no"));
 		}else if(user.getAuthority().equals("ROLE_PRO")){
 			session.setAttribute("pro_lec_no", pro_lec_no);
-			System.out.println("교수");
-			System.out.println(session.getAttribute("pro_lec_no"));
 		}
 		
 		if(pro_lec_no == null){
